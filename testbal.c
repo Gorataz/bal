a1e9eaf8d26195e6754df122b7d3e9194b9dac3a
Gorataz <32944170+Gorataz@users.noreply.github.com>
2019-03-19 13:44:01 +0100

    Update testbal.c

--- testbal.c
+++ testbal.c
@@ -41,17 +41,22 @@ int File_lettre_Vide(struct File_lettre *LET)
 }
 
 // ajoute une bal dans la BAL (file de boite aux lettres)
-int push_bal(struct bal_i *file, struct File_bal *BAL)  
+int push_bal(struct bal_i *element, struct File_bal *BAL)  
 {
     
-    if (File_bal_Vide(file))
+    if (File_bal_Vide(BAL))  
+    /*
+    Attention ici il y avait : if(File_bal_vide(element)) or element est une structure de type
+    struct bal_i* et File_bal_Vide demande une structure de type struct File_bal*
+    Il a donc fallut remplacer element par BAL
+    */
     {
         // si la file est vide, premier et dernier pointent sur le meme element
 
         BAL->dernier=malloc(sizeof(struct bal_i));
-        BAL->dernier=file;
-        file->premiere_lettre= NULL;
-        file->bal_suivant=NULL;
+        BAL->dernier=element;
+        element->premiere_lettre= NULL;
+        element->bal_suivant=NULL;
         BAL->premier=BAL->dernier;
         printf("vide\n");
     }
@@ -62,8 +67,8 @@ int push_bal(struct bal_i *file, struct File_bal *BAL)
         struct bal_i *nouveau;
         nouveau=BAL->dernier; // struct temporaire qui garde le dernier element de la liste
         BAL->dernier=malloc(sizeof(struct bal_i));
-        file->premiere_lettre= NULL;
-        file->bal_suivant=NULL;
+        element->premiere_lettre= NULL;
+        element->bal_suivant=NULL;
         nouveau->bal_suivant=BAL->dernier;
         printf("pas vide\n");
     }
@@ -96,27 +101,62 @@ int push_lettre(struct lettre *file, struct File_lettre *LET)
 
 
 // on recupere toutes les lettres, on affiche leur message et on supprime l'espace memoire de la lettre
-int pop_lettre (struct File_lettre *LET)
+void pop_lettre (struct File_lettre *LET)
 {
     LET->courant=LET->premier;
     struct lettre *elmout; 
-    while (LET->courant!=NULL) //Il y avait une erreur dans la version précédente : courant=!NULL a la place de courant!=NULL, je pense que c'est ca qui deconnait
+    while (LET->courant!=NULL) 
+    //Il y avait, je pense, une erreur dans la version précédente : courant=!NULL a la place de courant!=NULL
     {
         printf("%s\n",LET->courant->message);
         elmout=LET->courant;
         LET->courant=LET->courant->suivant;
-        free(elmout); 
+        free(elmout);
     }
 }
 
 void main() 
 {
-    struct bal_i *b; //que faire ici ?
-    b->i=0; //Un numero de bal a 0 (pour un test) ? Ou c'est une initialisation ?
-    b->bal_suivant=NULL; 
-    b->premiere_lettre=NULL;
+    // quoi faire ?
+    //Etape 1 : Creer une bal
+    //Etape 2 : Creer une lettre pour la bal 1
+    
+    
+    //Realisation 
+    //Etape 1 - creation d'une bal
+    struct File_bal Ensemble;
+    struct bal_i BAL;
+    BAL.i=1; //Il y aura un autre moyen de changer 'automatiquement' cette valeur -> cf recuperation des options dans le terminal
+    Creer_file_bal(&Ensemble); //Creation de la structure de BALs
+    push_bal(&BAL,&Ensemble); //On a push dans la file de BALs une premiere BAL
+    //Donc normalement ici on a une bal dans une file.
+    //On cherche maintenant a creer une lettre et la push dans cette bal
+    //
+    //Etape 2
+    //
+    struct File_lettre ssEnsemble1;
+    struct lettre lettre1;
+    lettre1.message="coucou";
+    Creer_file_lettre(&ssEnsemble1,&BAL); //syntaxe correcte est peut-etre Creer_file_lettre(struct File_lettre* ssEnsemble1, struct bal_i* BAL1); ?
+    push_lettre(&lettre1,&ssEnsemble1);
+    //Verifions maintenant que tout ce passe bien:
+    //
+    //Verifications
+    //
 
-    struct File_bal *BAL;
-    Creer_file_bal(BAL);
-    //push_bal(b,BAL) ;
+    printf("%s\n",ssEnsemble1.premier->message);
+    /*
+    Pb ici : on a rien après Ensemble.premier. ce qui veut dire qu'il n'y a pas de liens creer 
+    entre la structures de boites aux lettres et un element de cette structure.
+    -> Ou est-ce que ce lien est cense se faire ? =
+    Je n'ai peut etre pas fait de push ? :o bah si en fait j'en ai fait un
+    Ok donc finalement il faut faire un '->' car c'est un pointeur : on fait pointer vers 
+    un element distant. (Pourquoi un '.' avant alors ?)
+    Premier essai pour recuperer le message : erreur de segmentation ; faut il faire un
+    pop ou peut-on recuperer directement ?
+    Deuxieme essai : on recupere le numero de la lettre(?) ca marche !
+    Troisieme essai : toujours une erreur de segmentation.
+    Quatrieme essai : en passant par ssEnsemble1.premier->message ca marche !
+    */
+    printf("%s\n",lettre1.message);
 }
